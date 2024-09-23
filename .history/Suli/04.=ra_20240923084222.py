67e10ec1b4d4b0a8553d0338839b2f55b00eac4b
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-09-23 08:42:51 +0200

    Szöveg
    Szöveg

---
+++ .history/Suli/04.=ra_20240923084222.py
@@ -0,0 +1,7 @@
+"""
+
+while ciklus:
+    Egymáss után többször rlvégez egy művletet (mint a for)
+    Addig fut a ciklus amíg újra és újra teljesül a benne megfogalmazott logikai feltétel (mit egy ifben)
+
+"""
---
+++ Suli/04.=ra.py
@@ -0,0 +1,6 @@
+"""
+
+while ciklus:
+    Egymáss után többször rlvégez egy művletet (mint a for)
+    Addig fut a ciklus amíg újra és újra teljesül a benne megfogalmazott logikai feltétel (mit egy ifben)
+"""
