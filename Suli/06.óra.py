a65c5402cbc66ff0af9102555a66cf3ea115dcb8
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-10-07 10:20:15 +0200

    07.óra
    07.óra

--- Suli/06.óra.py
+++ Suli/06.óra.py
@@ -9,7 +9,7 @@ def mondatos(mondat):
 
 #print(mondatos("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et ante lorem. Aenean consequat egestas nisl, id facilisis nunc placerat eget. Proin nec congue sem"))
 
-#-------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
 
 szo = 'alma'
 szo = szo.replace('a', 'x')
@@ -30,6 +30,8 @@ def cenzura(mondat):
 
 #print(cenzura('Ez egy példa mondat, consectetur'))
 
+#----------------------------------------------------------------------------------------------------------------
+
 def cenzura2(mondat):
     szavak = mondat.split()
     for i in range(len(szavak)):
@@ -38,3 +40,28 @@ def cenzura2(mondat):
     return " ".join(szavak)
 
 #print(cenzura('Ez egy példa mondat, consectetur'))
+
+#----------------------------------------------------------------------------------------------------------------
+
+ertek = 4
+ertek2 = 5
+szoveg = "A tárolt tartalom az {} és {}".format(ertek, ertek2)
+szoveg2 = f"A tárolt tartalom az {ertek} és {ertek2}"
+
+print(szoveg)
+print(szoveg2)
+
+lista1 = 'abcd'
+lista2 = lista1[2:]
+
+print(lista2)
+
+#----------------------------------------------------------------------------------------------------------------
+
+def penzes(regi, uj):
+    regi = int(regi[1:])
+    uj = int(regi[1:])
+    szazalek = int(100*uj/regi-100)
+    return '{}%-os,'.format(abs(szazalek), 'növekedés' if szazalek > 0 else 'csökkenés')
+
+print(penzes('$400','É700'))
