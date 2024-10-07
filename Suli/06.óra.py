e254699dda2260ebc28060d77f20fc7eefba2474
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-10-07 09:24:37 +0200

    2. megoldás
    2. megoldás

--- Suli/06.óra.py
+++ Suli/06.óra.py
@@ -25,7 +25,16 @@ Feladat: Ez egy példa mondat --> Ez egy ***** ******
 def cenzura(mondat):
     for szo in mondat.split():
         if len(szo) > 4:
-            mondat = mondat.replace(szo, "*"*len(szo))
+            mondat = mondat.replace(szo, "*" * len(szo))
         return mondat
 
-#print(cenzura('Ez egy példa mondat'))
+print(cenzura('Ez egy példa mondat, consectetur'))
+
+def cenzura2(mondat):
+    szavak = mondat.split()
+    for i in range(len(szavak)):
+        if len(szavak[i] > 4):
+            szavak[i] = "*" * len(szavak[i])
+    return " ".join(szavak)
+
+print(cenzura('Ez egy példa mondat, consectetur'))
