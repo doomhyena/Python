b7864735aee482469c4e2b451d8838d78a5e95eb
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-13 18:28:26 +0200

    Madlibs Game
    Madlibs Game :3

---
+++ Nem Suli/madlibs.py
@@ -0,0 +1,11 @@
+adjective1 = input('Enter and  adjective: ')
+noun1 = input('Enter a noun: ')
+adjective2 = input('Enter an adjective: ')
+verb1 = input('Enter a verb: ')
+adjective3 = input('Enter an adjective: ')
+
+print(f"Today I wnet to a {adjective1} zoo.")
+#print(f"Today I saw a {adjective2} {animal}.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
