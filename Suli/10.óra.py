948d8eaa51c2ff6e352061e0870abcaf358a4637
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-11-04 10:57:22 +0100

    class Beteg
    class Beteg

--- Suli/10.óra.py
+++ Suli/10.óra.py
@@ -1,4 +1,4 @@
-class Tanulo:
+"""class Tanulo:
     def __init__(self, id, nev, evfolyam, kepzes):
         self.id = id
         self.nev = nev
@@ -11,10 +11,10 @@ class Tanulo:
         return Tanulo(id, nev, evfolyam, kepzes)
 
 tanulo = Tanulo(4, "Csontos Kincső", 13, "Szoftverfejlesztő- és Tesztelő")
-tt = Tanulo.tagolt("04/Csontos Kincső/13/Szoftverfejlesztő- és Tesztelő")
+tt = Tanulo.tagolt("04-Kincső-13-Szoftverfejlesztő- és Tesztelő")
 
-print(tt)
-print(f" {tanulo.id} -> {tanulo.nev}, {tanulo.evfolyam} évfolyamba jár, és {tanulo.kepzes}-nak/nek tanul.")
+#print(tt)
+print(f" {tanulo.id} -> {tanulo.nev}, {tanulo.evfolyam} évfolyamba jár, és {tanulo.kepzes}-nak/nek tanul.")"""
 
 
 class Beteg:
@@ -24,9 +24,11 @@ class Beteg:
         self.panasz = panasz
         self.diagnozis = diagnozis
     def kiiras(self):
-        return self.szig
+        return f"{Beteg.nev} beteget {Beteg.panasz} panasszal hozták be és kiderült, hogy {Beteg.diagnozis}-e van."
     def tagolt(adat):
         szig, nev, panasz, diagnozis = (adat).split("/")
         return Beteg(szig, nev, panasz, diagnozis)
 
+#b = Beteg.tagolt('1-Béla-Hasfájás-Gyomorrontás')
+#print(b.panasz)
 
